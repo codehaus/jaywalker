827d10891a804d294de5193b75653594c8a77bf5
raniemi <raniemi@f90e38ac-8d02-0410-930a-c54d68672637>
2006-01-25 03:54:52 +0000

    Added missing file

---
+++ src/app/jaywalker/report/Report.java
@@ -0,0 +1,43 @@
+package jaywalker.report;
+
+import java.io.ByteArrayOutputStream;
+import java.net.URL;
+import java.util.Stack;
+
+import jaywalker.util.XsltTransformer;
+import jaywalker.xml.Tag;
+
+public class Report {
+
+	private final Tag[] tags;
+
+	private final XsltTransformer[] transformers;
+
+	public Report(Tag[] tags, XsltTransformer[] transformers) {
+		this.tags = tags;
+		this.transformers = transformers;
+	}
+
+	public String toTagString(URL url, Stack parentUrlStack) {
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < tags.length; i++) {
+			sb.append(tags[i].create(url, parentUrlStack));
+		}
+		return sb.toString();
+	}
+
+	public String[] transform() {
+		String[] values = new String[transformers.length];
+		for (int i = 0; i < transformers.length; i++) {
+			if (transformers[i] != null) {
+				ByteArrayOutputStream baos = new ByteArrayOutputStream();
+				transformers[i].write(baos);
+				values[i] = baos.toString();
+			} else {
+				values[i] = "";
+			}
+		}
+		return values;
+	}
+
+}
