ba9c334f3865977f4d316d54cd57eefc6b7ed7a4
raniemi <raniemi@f90e38ac-8d02-0410-930a-c54d68672637>
2006-01-25 05:25:28 +0000

    Added ConfigurationSetup

--- src/app/jaywalker/report/ReportExecutor.java
+++ src/app/jaywalker/report/ReportExecutor.java
@@ -19,14 +19,11 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Writer;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -34,53 +31,25 @@ import java.util.Set;
 
 import jaywalker.classlist.ClasslistElement;
 import jaywalker.classlist.ClasslistElementFactory;
-import jaywalker.classlist.ClasslistElementListener;
 import jaywalker.classlist.ClasslistElementStatistic;
 import jaywalker.classlist.ClasslistElementVisitor;
 import jaywalker.util.FileSystem;
 import jaywalker.util.ResourceLocator;
-import jaywalker.util.XsltTransformer;
-import jaywalker.xml.Tag;
 
 public class ReportExecutor {
 
-	private final Configuration[] configurations = new Configuration[] {
-		new DependencyReportConfiguration(new DependencyModel()),
-		new CollisionReportConfiguration(new CollisionModel()) };
-	
+	private ConfigurationSetup configurationSetup = new ConfigurationSetup();
+
 	private ClasslistElementFactory factory = new ClasslistElementFactory();
 
 	public void execute(String classlist, Properties properties, File outDir)
 			throws IOException {
 		initOutDir(outDir);
-		Report[] reports = toReports(properties);
+		Report[] reports = configurationSetup.toReports(properties);
 		AggregateReport report = execute(classlist, reports);
 
 		outputXml(outDir, report);
 		outputHtml(outDir, reports);
-	}
-	
-	private String[] getReportTypes() {
-		List reportTypeList = new ArrayList();
-		for (int i = 0; i < configurations.length; i++) {
-			reportTypeList.addAll(Arrays.asList(configurations[i]
-					.getReportTypes()));
-		}
-		return (String[]) reportTypeList.toArray(new String[reportTypeList
-				.size()]);
-	}
-
-	private Report[] toReports(Properties properties) {
-		List reportList = new LinkedList();
-		for (int i = 0; i < configurations.length; i++) {
-			Tag[] reportTags = configurations[i].toReportTags(properties);
-			if (reportTags.length > 0) {
-				XsltTransformer[] transformers = configurations[i]
-						.toXsltTransformers(properties);
-				reportList.add(new Report(reportTags, transformers));
-			}
-		}
-		return (Report[]) reportList.toArray(new Report[reportList.size()]);
 	}
 
 	private void outputXml(File outDir, AggregateReport report)
@@ -115,7 +84,10 @@ public class ReportExecutor {
 			throws IOException {
 		final ClasslistElement[] elements = factory.create(classlist);
 		initReportModels(reports, elements);
+		return createAggregateReport(reports, elements);
+	}
 
+	private AggregateReport createAggregateReport(Report[] reports, final ClasslistElement[] elements) throws IOException {
 		ClasslistElementVisitor visitor = new ClasslistElementVisitor(elements);
 		AggregateReport report = new AggregateReport(reports);
 		visitor.addListener(report);
@@ -123,21 +95,14 @@ public class ReportExecutor {
 		return report;
 	}
 
-	private ClasslistElementListener[] getClasslistElementListeners() {
-		ClasslistElementListener[] listeners = new ClasslistElementListener[configurations.length];
-		for (int i = 0; i < configurations.length; i++) {
-			listeners[i] = configurations[i].getClasslistElementListener();
-		}
-		return listeners;
-	}
-	
 	private void initReportModels(Report[] reports,
 			final ClasslistElement[] elements) throws IOException {
 		ClasslistElementVisitor visitor = new ClasslistElementVisitor(elements);
 		final ClasslistElementStatistic statisticListener = new ClasslistElementStatistic();
 		visitor.addListener(statisticListener);
-		
-		AggregateModel model = new AggregateModel(getClasslistElementListeners());
+
+		AggregateModel model = new AggregateModel(configurationSetup
+				.getClasslistElementListeners());
 		visitor.addListener(model);
 		Date start = new Date();
 		visitor.accept();
@@ -146,41 +111,9 @@ public class ReportExecutor {
 		System.out.println(statisticListener + " instrumented.");
 		visitor.removeAllListeners();
 	}
-
+	
 	public String[] getReportDescriptions() {
-		String[] reportTypes = getReportTypes();
-		Map map = toReportTypeMap(reportTypes);
-		Set keySet = map.keySet();
-		List descriptionList = new ArrayList();
-		for (Iterator itKey = keySet.iterator(); itKey.hasNext();) {
-			final Object key = itKey.next();
-			Set valueSet = (Set) map.get(key);
-			StringBuffer sb = new StringBuffer();
-			sb.append(key).append("=");
-			for (Iterator itValue = valueSet.iterator(); itValue.hasNext();) {
-				sb.append(itValue.next());
-				if (itValue.hasNext())
-					sb.append(",");
-			}
-			descriptionList.add(sb.toString());
-		}
-		return (String[]) descriptionList.toArray(new String[descriptionList
-				.size()]);
-	}
-
-	private Map toReportTypeMap(String[] reportTypes) {
-		Map map = new HashMap();
-		for (int i = 0; i < reportTypes.length; i++) {
-			String reportType = reportTypes[i];
-			String[] reportTypeAttributes = reportType.split(",");
-			Set set = (Set) map.get(reportTypeAttributes[0]);
-			if (set == null) {
-				set = new HashSet();
-				map.put(reportTypeAttributes[0], set);
-			}
-			set.add(reportTypeAttributes[1]);
-		}
-		return map;
+		return configurationSetup.getReportDescriptions();
 	}
 
 }
